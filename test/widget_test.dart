940970526ab43ebacdde6185e7d5de19e1f299a0
Unknown-bot-sus <albent2017@gmail.com>
2022-03-14 22:37:56 +0630

    '###'

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:duwin/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('home page is created', (WidgetTester tester) async {
+    final testWidget = MaterialApp(
+      home: Text('hello'),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.pumpWidget(testWidget);
+    await tester.pumpAndSettle();
   });
 }
